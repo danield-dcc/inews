9141aed8ad7bfcf1dae3d5bc048fb63c8bf74018
Daniel Conceição <daniel.colares@gmail.com>
2022-04-03 10:44:24 -0300

    ✨ add integration with stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,47 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
+//3 lugares do next-auth que pode user as chaves de segurança
+//getServerSideProps (SSR)
+//getStaticProps (SSR)
+//API Routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+    const { data: session } = useSession();
+
+    async function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return;
+        }
+
+        //criação da checkout session
+        try {                                   //aqui a rota é o nome do arquivo
+            const response = await api.post('/subscribe');
+
+            const { sessionId } = response.data;
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({sessionId: sessionId});
+
+        } catch (error) {
+            console.log(error);
+            alert(error.message);
+        }
+    }
+
     return (
         <button
         type="button"
         className={styles.subscribeButton}
+        onClick={handleSubscribe}
         >
             Subscribe now
         </button>
